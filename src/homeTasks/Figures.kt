938de4cf5891a9e3fb41a1121967a5609a119269
Alex <1965167lol@gmail.com>
2025-07-08 13:09:54 +0300

    Добавил в Figures асбтрактный класс Shape, чтобы от него наследовать функции расчета площади, периметра, которые возвращают округленные значения.

--- src/homeTasks/Figures.kt
+++ src/homeTasks/Figures.kt
@@ -1,2 +1,52 @@
 package homeTasks
 
+import kotlin.math.round
+import kotlin.math.PI
+
+abstract class Shape(val name: String){
+
+    abstract fun area(): Double
+    abstract fun perimeter(): Double
+
+    protected fun roundToTwoDecimals(value: Double): Double{
+        val roundedValue = round(value * 100) / 100
+        return roundedValue
+    }
+}
+
+class Circle(
+    name: String,
+    val radius: Double
+): Shape(name) {
+
+    override fun area(): Double {
+        val area = PI * radius * radius
+        roundToTwoDecimals(area)
+        return area
+    }
+
+    override fun perimeter(): Double {
+        val perimeter = 2 * PI * radius
+        roundToTwoDecimals(perimeter)
+        return perimeter
+    }
+}
+
+class Rectangle(
+    name: String,
+    val width: Double,
+    val height: Double
+): Shape(name) {
+
+    override fun area(): Double {
+        val area = width * height
+        roundToTwoDecimals(area)
+        return area
+    }
+
+    override fun perimeter(): Double {
+        val perimeter = 2 * (width + height)
+        roundToTwoDecimals(perimeter)
+        return perimeter
+    }
+}
